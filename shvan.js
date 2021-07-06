8230d01f8d4082f9ae6a6eaebd9d11a5b271e60c
LAWAN2 <75916704+LAWAN2@users.noreply.github.com>
2021-07-06 12:30:17 +0300

    Update shvan.js

--- shvan.js
+++ shvan.js
@@ -7387,7 +7387,7 @@ message.member.voiceChannel
 
 //تۆكینه‌كان لێره‌ جا بنێ
 
-client.login("ODM1Njg5MjUwODI5NjMxNDg5.YIeupQ.godag4QNNo2hd4btc1i-102XPp4");
+client.login("ODU5MTQ5NzQwODY0NDM4Mjgz.YNrBBQ.f2e6OEt3cIhDbUNE6ov-5TQ5da4");
 client2.login("");
 client3.login("");
 client4.login("");
